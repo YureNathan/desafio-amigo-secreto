8ce32c08b395cab59a4499b6d558f1559dfeecdc
Yure <yure.nathan303@gmail.com>
2025-09-02 15:30:26 -0300

    Adicionando funcionalidade de mostrar nome dos amigos na tela inicial

--- app.js
+++ app.js
@@ -8,5 +8,13 @@ function adicionarAmigo() {
     amigos.push(nomeAmigo);
   }
   document.getElementById("amigo").value = "";
-  console.log(amigos);
+  atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`;
+  }
 }
